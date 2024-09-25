3777cdfc2ab892f31513dfc83c1869c7ab2bf891
shriramrbk <shriramrbk@gmail.com>
2024-09-25 19:57:12 +0530

    Basic interaction between all the components - Client, Master and ChunkServer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const { MasterServer } = require("./masterServer")
-const { ChunkServer } = require("./chunkServer")
+const { ChunkServerV2 } = require("./chunkServerV2")
+const { Client } = require("./client")
+
+const path = require("path");
 
 const main = () => {
 
@@ -8,18 +11,20 @@ const main = () => {
   console.log("masterServer.filesCollection = ", masterServer.filesCollection)
 
   // init chunk server 1
-  const chunkServer1 = new ChunkServer("ID-1", masterServer)
+  const chunkServer1 = new ChunkServerV2("ID-1", masterServer)
+  masterServer.registerChunkServer(chunkServer1)
 
-  // First file to chunk and store
-  const sourceFilePath1 = "sourceFile.md"
-  chunkServer1.chunk(sourceFilePath1).then( res => {
-    console.log("chunkServer1.chunk response = ", res)
-    console.log("masterServer.filesCollection = ", masterServer.filesCollection) 
+  // init chunk server 2
+  const chunkServer2 = new ChunkServerV2("ID-2", masterServer)
+  masterServer.registerChunkServer(chunkServer2)
 
-    // regenerate the file from chunks
-    chunkServer1.read(sourceFilePath1)
-  })
-
+  // init Client
+  const client = new Client("1", masterServer)
+  
+  // start file upload from client
+  const sourceFileName = "sourceFile.md"
+  const sourceFilePath = path.join(__dirname, sourceFileName)
+  client.storeFile(sourceFilePath)
 }
 
 main()
